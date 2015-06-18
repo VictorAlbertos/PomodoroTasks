9b05f8717f7c352e26ca0ce6bd938820637e9dc8
Victor <victor@refineriaweb.com>
2015-06-19 00:25:36 +0200

    basic setup

---
+++ app/src/main/java/activities/LaunchActivity.java
@@ -0,0 +1,10 @@
+package activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class LaunchActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/utilities/PomodoroApp.java
@@ -0,0 +1,9 @@
+package utilities;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+@EApplication
+public class PomodoroApp extends Application {
+}
